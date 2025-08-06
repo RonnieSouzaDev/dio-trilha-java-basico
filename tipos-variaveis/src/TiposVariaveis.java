76259eb2a9e9416a7aac6be5e0690d8d2272b532
ronnie.souza <ronnie.souza@objective.com.br>
2025-08-05 21:07:49 -0300

    Adicionado exemplos de tipos e variaveis em java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,33 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Tipos de Variáveis");
+
+        int numero = 5;
+        float decimal = 5.5f;
+        char letra = 'A';
+        boolean verdadeiro = true;
+        String texto = "Olá, Mundo!";
+        long grandeNumero = 100000L;
+        double salario = 2500.75;
+
+        System.out.println(numero);
+        System.out.println(decimal);
+        System.out.println(letra);
+        System.out.println(verdadeiro);
+        System.out.println(texto);
+        System.out.println(grandeNumero);
+        System.out.println(salario);
+
+        short numeroPequeno = 10;
+        int numeroNormal = numeroPequeno;
+        short numeroCurto = (short) numeroNormal;
+        numero = 10;
+
+        System.out.println(numero);
+        System.out.println(numeroPequeno);
+        System.out.println(numeroNormal);
+        System.out.println(numeroCurto);
+
+        final double VALOR_DE_PI = 3.14159;
+        System.out.println("Valor de PI: " + VALOR_DE_PI);
     }
 }
